1838227740757d1d6ed5b6d974211157e4efeeab
Waldos Mendoza <mendozawaldos@hotmail.com>
2020-06-10 21:03:45 -0600

    to do

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
